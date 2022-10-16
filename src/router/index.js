af92517cd86e0356daa08dbb4247ee67209c7061
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2022-10-16 22:19:13 +0300

    added lifecycle hooks page

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/lifecycle",
+    name: "LifeCycleHooks",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/LifeCycleHooks.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/LifeCycleHooks.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, onMounted, onUpdated, onUnmounted } from "vue";
+
+const el = ref();
+const buttonCountRef = ref();
+const count = ref(1);
+let intervalId;
+
+onMounted(() => {
+  el.value; // <div>
+  intervalId = setInterval(() => {
+    // ...
+  });
+  console.log("____onMounted____ template element --> ", el.value);
+});
+onUpdated(() => {
+  console.log(
+    "____onUpdated____ button text --> ",
+    buttonCountRef.value.textContent
+  );
+});
+onUnmounted(() => {
+  clearInterval(intervalId);
+  console.log("____onUnmounted____ About Page has been unmounted");
+});
+</script>
+<template>
+  <div ref="el">
+    <h1>This is A Demo Page for Lifecycle Hooks Training</h1>
+    <button ref="buttonCountRef" @click="count++">counter {{ count }}</button>
+  </div>
+</template>
